c7ce24ad75a441701d9c0e53ffe29108f9b0d0ff
gbergy <greg.goldberg718@gmail.com>
2016-07-06 17:40:50 -0400

    splash page and map zoom

--- app/src/main/java/com/cs6392016/gbergy/stooperapp/MapsActivity.java
+++ app/src/main/java/com/cs6392016/gbergy/stooperapp/MapsActivity.java
@@ -1,22 +1,22 @@
 package com.cs6392016.gbergy.stooperapp;
 
-import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.os.AsyncTask;
-import android.support.v4.app.ActivityCompat;
+import android.content.pm.PackageManager; //
+
+import android.Manifest; //
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat; //
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.os.Bundle; //
+import android.support.v4.content.ContextCompat; //
+import android.view.View; //
+import android.widget.CheckBox;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.GoogleMap; //
+import com.google.android.gms.maps.OnMapReadyCallback; //
+import com.google.android.gms.maps.SupportMapFragment; //
+import com.google.android.gms.maps.UiSettings; //
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -26,11 +26,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+    private UiSettings mUiSettings;
+
+    private CheckBox mMyLocationButtonCheckbox;
+
+    private CheckBox mMyLocationLayerCheckbox;
+
+    private static final int MY_LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    private static final int LOCATION_LAYER_PERMISSION_REQUEST_CODE = 2;
+
+    /**
+     * Flag indicating whether a requested permission has been denied after returning in
+     * {@link #onRequestPermissionsResult(int, String[], int[])}.
+     */
+    private boolean mLocationPermissionDenied = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -38,17 +55,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
+     * Returns whether the checkbox with the given id is checked.
      */
+    private boolean isChecked(int id) {
+        return ((CheckBox) findViewById(id)).isChecked();
+    }
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        mUiSettings = mMap.getUiSettings();
+
+        // Keep the UI Settings state in sync with the checkboxes.
+        mUiSettings.setZoomControlsEnabled(isChecked(R.id.zoom_buttons_toggle));
+        mUiSettings.setCompassEnabled(isChecked(R.id.compass_toggle));
+        mUiSettings.setMyLocationButtonEnabled(isChecked(R.id.mylocationbutton_toggle));
+        //mMap.setMyLocationEnabled(isChecked(R.id.mylocationlayer_toggle));
+        mUiSettings.setScrollGesturesEnabled(isChecked(R.id.scroll_toggle));
+        mUiSettings.setZoomGesturesEnabled(isChecked(R.id.zoom_gestures_toggle));
+        mUiSettings.setTiltGesturesEnabled(isChecked(R.id.tilt_toggle));
+        mUiSettings.setRotateGesturesEnabled(isChecked(R.id.rotate_toggle));
 
         // Add a marker in Park Slope and move the camera
         LatLng parkSlope = new LatLng(40.6710403, -73.9988001);
@@ -69,4 +97,164 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
+    /**
+     * Checks if the map is ready (which depends on whether the Google Play services APK is
+     * available. This should be called prior to calling any methods on GoogleMap.
+     */
+    private boolean checkReady() {
+        if (mMap == null) {
+            Toast.makeText(this, R.string.map_not_ready, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    public void setZoomButtonsEnabled(View v) {
+        if (!checkReady()) {
+            return;
+        }
+        // Enables/disables the zoom controls (+/- buttons in the bottom-right of the map for LTR
+        // locale or bottom-left for RTL locale).
+        mUiSettings.setZoomControlsEnabled(((CheckBox) v).isChecked());
+    }
+
+    public void setCompassEnabled(View v) {
+        if (!checkReady()) {
+            return;
+        }
+        // Enables/disables the compass (icon in the top-left for LTR locale or top-right for RTL
+        // locale that indicates the orientation of the map).
+        mUiSettings.setCompassEnabled(((CheckBox) v).isChecked());
+    }
+
+    public void setMyLocationButtonEnabled(View v) {
+        if (!checkReady()) {
+            return;
+        }
+        // Enables/disables the my location button (this DOES NOT enable/disable the my location
+        // dot/chevron on the map). The my location button will never appear if the my location
+        // layer is not enabled.
+        // First verify that the location permission has been granted.
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mUiSettings.setMyLocationButtonEnabled(mMyLocationButtonCheckbox.isChecked());
+        } else {
+            // Uncheck the box and request missing location permission.
+            mMyLocationButtonCheckbox.setChecked(false);
+            requestLocationPermission(MY_LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    public void setMyLocationLayerEnabled(View v) {
+        if (!checkReady()) {
+            return;
+        }
+        // Enables/disables the my location layer (i.e., the dot/chevron on the map). If enabled, it
+        // will also cause the my location button to show (if it is enabled); if disabled, the my
+        // location button will never show.
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(mMyLocationLayerCheckbox.isChecked());
+        } else {
+            // Uncheck the box and request missing location permission.
+            mMyLocationLayerCheckbox.setChecked(false);
+            PermissionUtils.requestPermission(this, LOCATION_LAYER_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, false);
+        }
+    }
+
+    public void setScrollGesturesEnabled(View v) {
+        if (!checkReady()) {
+            return;
+        }
+        // Enables/disables scroll gestures (i.e. panning the map).
+        mUiSettings.setScrollGesturesEnabled(((CheckBox) v).isChecked());
+    }
+
+    public void setZoomGesturesEnabled(View v) {
+        if (!checkReady()) {
+            return;
+        }
+        // Enables/disables zoom gestures (i.e., double tap, pinch & stretch).
+        mUiSettings.setZoomGesturesEnabled(((CheckBox) v).isChecked());
+    }
+
+    public void setTiltGesturesEnabled(View v) {
+        if (!checkReady()) {
+            return;
+        }
+        // Enables/disables tilt gestures.
+        mUiSettings.setTiltGesturesEnabled(((CheckBox) v).isChecked());
+    }
+
+    public void setRotateGesturesEnabled(View v) {
+        if (!checkReady()) {
+            return;
+        }
+        // Enables/disables rotate gestures.
+        mUiSettings.setRotateGesturesEnabled(((CheckBox) v).isChecked());
+    }
+
+    /**
+     * Requests the fine location permission. If a rationale with an additional explanation should
+     * be shown to the user, displays a dialog that triggers the request.
+     */
+    public void requestLocationPermission(int requestCode) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Display a dialog with rationale.
+            PermissionUtils.RationaleDialog
+                    .newInstance(requestCode, false).show(
+                    getSupportFragmentManager(), "dialog");
+        } else {
+            // Location permission has not been granted yet, request it.
+            PermissionUtils.requestPermission(this, requestCode,
+                    Manifest.permission.ACCESS_FINE_LOCATION, false);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == MY_LOCATION_PERMISSION_REQUEST_CODE) {
+            // Enable the My Location button if the permission has been granted.
+            if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                mUiSettings.setMyLocationButtonEnabled(true);
+                mMyLocationButtonCheckbox.setChecked(true);
+            } else {
+                mLocationPermissionDenied = true;
+            }
+
+        } else if (requestCode == LOCATION_LAYER_PERMISSION_REQUEST_CODE) {
+            // Enable the My Location layer if the permission has been granted.
+            if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                mMap.setMyLocationEnabled(true);
+                mMyLocationLayerCheckbox.setChecked(true);
+            } else {
+                mLocationPermissionDenied = true;
+            }
+        }
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mLocationPermissionDenied) {
+            PermissionUtils.PermissionDeniedDialog
+                    .newInstance(false).show(getSupportFragmentManager(), "dialog");
+            mLocationPermissionDenied = false;
+        }
+    }
 }
